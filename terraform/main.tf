1437389de5a06afe8edf648e38441da983287334
Ru <ruhuang1107@gmail.com>
2026-08-26 12:42:14 +0000

    fix(terraform): configure ingress health probe path, cluster traffic policy, and replica count

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,4 +115,16 @@ resource "helm_release" "ingress_nginx" {
     name  = "controller.service.type"
     value = "LoadBalancer"
   }
+  set {
+    name  = "controller.replicaCount"
+    value = "2"
+  }
+  set {
+    name  = "controller.service.externalTrafficPolicy"
+    value = "Cluster"
+  }
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-health-probe-request-path"
+    value = "/healthz"
+  }
 }
